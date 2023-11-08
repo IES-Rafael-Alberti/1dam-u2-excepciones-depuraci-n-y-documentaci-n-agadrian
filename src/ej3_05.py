023aaa041f86d5d274d73fc10ebfd3af46a4592e
agadrian <agadrian@iesrafaelalberti.es>
2023-11-08 19:32:47 +0100

    push

--- src/ej3_05.py
+++ src/ej3_05.py
@@ -4,7 +4,7 @@ Ejercicio 2.3.5
 Escribir que solicite una contraseña, y si no coincide con la que se tiene, lance la excepción NameError con el mensaje, "Incorrect Password!!"
 """
 
-def comprobarContraseña(contraseña1:str, contraseña2:str) -> bool:
+def comprobarContraseña(contraseña1:str, contraseña2:str):
     """
     Comprueba si las contraseñas coinciden, si no lanza excepcion NameError
 
@@ -12,11 +12,10 @@ def comprobarContraseña(contraseña1:str, contraseña2:str) -> bool:
         contraseña1: str 
         contraseña2: str
     """
-    
+
     if contraseña1 != contraseña2:
         raise NameError
     
-   
 
 def main():
     try:
@@ -26,6 +25,7 @@ def main():
         print("Coinciden")
     except NameError:
         print("Incorrect Password!!")
+       
 
 
 if __name__ == "__main__":
---
+++ src/ej4_02.py
@@ -0,0 +1,10 @@
+#### ALGORITMO BURBUJA ####
+
+# BULCE PADRE HACE LAS PASADAS range (0, len(lista)) 
+#BUCLE HIJO HACE una pasada menos del total de elementos de la lista (0, len(lista) - 1)
+"""
+LISTA = ..........
+lsitaOrdenada = ordenarlista(lista)
+obtenerListaNumeros (listaOrdenada)
+print(obtenerListaNumeros)
+"""
