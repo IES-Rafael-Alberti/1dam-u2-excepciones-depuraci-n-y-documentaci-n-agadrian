9ba3c4e7c0850b99db7eff217d8752b5e43881f5
agadrian <agadrian@iesrafaelalberti.es>
2023-11-08 19:45:29 +0100

    up

--- src/ej4_02.py
+++ src/ej4_02.py
@@ -8,3 +8,20 @@ lsitaOrdenada = ordenarlista(lista)
 obtenerListaNumeros (listaOrdenada)
 print(obtenerListaNumeros)
 """
+
+
+def ordenarLista(lista):
+    for i in range(0, len(lista)):
+        for j in range(0, len(lista)-1):
+            if lista[j] > lista[j+1]:
+                lista[j] == lista[j+1]
+    return lista
+
+def main():
+    lista = [1,4,6,2,10]
+    listaOrdenada = ordenarLista(lista)
+
+    print(listaOrdenada)
+
+if __name__ == "__main__":
+    main()
