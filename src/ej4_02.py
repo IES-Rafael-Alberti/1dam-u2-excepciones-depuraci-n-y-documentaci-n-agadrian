3cdde3e35a6214bb5b952fec34e132c800ba09be
agadrian <agadrian@iesrafaelalberti.es>
2023-11-09 21:02:33 +0100

    burbuja

--- src/ej4_02.py
+++ src/ej4_02.py
@@ -1,30 +1,54 @@
 #### ALGORITMO BURBUJA ####
 
-# BULCE PADRE HACE LAS PASADAS range (0, len(lista)) 
-#BUCLE HIJO HACE una pasada menos del total de elementos de la lista (0, len(lista) - 1)
-"""
-LISTA = ..........
-lsitaOrdenada = ordenarlista(lista)
-obtenerListaNumeros (listaOrdenada)
-print(obtenerListaNumeros)
-"""
+def ordenarLista(lista) -> list:
+    """
+    Ordena una lista numerica de menor a mayor
 
+    Args:
+        list: lista numerica desordenada
 
-def ordenarLista(lista):
+    Retorna:
+        list: ordenada de menor a mayor
+    """
     # Bucle padre e hijo se repetira 1 vez menos del total de numeros en la lista
-    for i in range(0, len(lista)-1):
-        for j in range(0, len(lista)-1):
-            if lista[j] > lista[j+1]:
+    for i in range(0, len(lista) - 1):
+        for j in range(0, len(lista) - 1):
+            if lista[j] > lista[j + 1]:
                 aux = lista[j]
-                lista[j] = lista[j+1]
-                lista[j+1] = aux
+                lista[j] = lista[j + 1]
+                lista[j + 1] = aux
     return lista
 
-def main():
-    lista = [1,8,6,2,9,3,10,7]
-    listaOrdenada = ordenarLista(lista)
 
-    print(listaOrdenada)
+def obtenerListaNumeros(lista):
+    """
+    Almacena en una variable, los numeros de forma mas legible, separados por comas
+
+    Args:
+        list: lista ordenada
+    
+    Retorna:
+        str: numeros de la ordenados seprados por coma 
+    """
+    ordenado = " "
+
+    for i in lista:
+        if i == lista[-1]:
+            ordenado += str(i)
+        else:
+            ordenado += str(i) + ", "
+    
+    return ordenado
+
+
+def main():
+    lista = [1,8,6,4,45,23,56,34,34]
+    listaOrdenada = ordenarLista(lista)
+    listaFormateada = (obtenerListaNumeros(listaOrdenada))
+
+    print(f"Lista final: {listaFormateada}")
+
+
 
 if __name__ == "__main__":
     main()
